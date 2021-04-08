b2dafacee096bca72bc62e709ad8b83ae2d087d8
Daniel812121 <58784062+Daniel812121@users.noreply.github.com>
2021-04-08 13:53:59 -0400

    d

--- commands.js
+++ commands.js
@@ -12,7 +12,8 @@ module.exports = async function (msg) {
       let tokens = msg.content.split(" ");
       let command = tokens.shift();
 
-      if (command.charAt(0) === "!") {
+      if (command.charAt(0) === "!") 
+      {
         command = command.substring(1);
         commands[command](msg, tokens);
       }
