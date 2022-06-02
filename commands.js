f808b5841978682bb5218d779836632f6f6d7fdb
Daniel K <dkaplan@aimpa.org>
2022-06-01 23:54:38 -0400

    Last Update

--- commands.js
+++ commands.js
@@ -4,8 +4,9 @@
 const gif = require("./commands/gif.js");
 const hello = require("./commands/hello.js");
 const play = require("./commands/play.js"); 
+const stop = require("./commands/play.js");
 
-const commands = { hello, gif, play };
+const commands = { hello, gif, play, stop };
 
 module.exports = async function (msg) {
   if (msg.channel.id == '823622800736124958' || msg.channel.id == '823895207556415518') {
--- commands/hello.js
+++ commands/hello.js
@@ -1,7 +1,7 @@
 
 const replies = [ 'Have I been summoned?', 'Why Hello There', 'Wwwwassssuuuppp','Hey 😀', '👋' ];
 
-module.exports = function(msg, args)
+module.exports = function(msg, args) 
 {
 const index = Math.floor(Math.random() * replies.length);
 msg.channel.send(replies[index]);
